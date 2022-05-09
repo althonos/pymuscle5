ae09463b5c46a0c6ec6fb40a5bdbd804058da96e
Martin Larralde <martin.larralde@embl.de>
2022-05-09 16:37:23 +0200

    Include additional tests for LuxC homolog proteins

--- pymuscle/tests/test_muscle.py
+++ pymuscle/tests/test_muscle.py
@@ -10,6 +10,30 @@ import pymuscle
 
 class TestAligner(unittest.TestCase):
 
+    def test_luxc(self):
+
+        with importlib_resources.path("pymuscle.tests.data", "LuxC.faa") as path:
+            sequences = pymuscle.MultiSequence.from_file(str(path))
+            self.assertEqual(len(sequences), 13)
+
+        aligner = pymuscle.Aligner()
+        msa = aligner.align(sequences)
+        self.assertEqual(len(msa.sequences), 13)
+
+        with importlib_resources.path("pymuscle.tests.data", "LuxC.muscle.afa") as path:
+            expected = pymuscle.MultiSequence.from_file(str(path))
+            self.assertEqual(len(expected), 13)
+
+        actual_sequences = { seq.name:seq for seq in msa.sequences }
+        expected_sequences = {seq.name:seq for seq in expected }
+
+        self.assertEqual(set(actual_sequences), set(expected_sequences))
+        for name in sorted(expected_sequences):
+            actual_seq = actual_sequences[name]
+            expected_seq = expected_sequences[name]
+            self.assertEqual(actual_seq.name.decode(), expected_seq.name.decode())
+            self.assertEqual(actual_seq.sequence.decode(), expected_seq.sequence.decode())
+
     def test_halorhodopsin(self):
 
         with importlib_resources.path("pymuscle.tests.data", "swissprot-halorhodopsin.faa") as path:
